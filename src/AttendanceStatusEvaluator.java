10d74ae357a2effba817bdabb06a6a7b875253d9
Nivaethika <nivaethika08@gmail.com>
2026-01-19 15:47:48 +0530

    Update AttendanceStatusEvaluator.java

--- src/AttendanceStatusEvaluator.java
+++ src/AttendanceStatusEvaluator.java
@@ -6,5 +6,17 @@ public class AttendanceStatusEvaluator {
 
         Scanner sc = new Scanner(System.in);
         int attendence=sc.nextInt();
-        if(attendence>85)
+        if(attendence>85){
+            System.out.println("Excellent");
+        }
+        else if(attendence<60){
+            System.out.println("Poor");
+        }
+        else{
+             System.out.println("Satisfactory");
+        }
+        sc.close();
+
+    }
+}
 
